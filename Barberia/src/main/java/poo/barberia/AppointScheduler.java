3de4110085b17658f6521f043a58cfe337ed4d2a
Nicole <nicolerodriguezluna@estudiantec.cr>
2024-04-22 20:15:41 -0600

    Funciones de clientes y lista de espera
    Mi parte casi completa, me falta la función de eliminar clientes pues es una dependencia de la clase cita que aún no existe en el proyecto. Este commit no incluye la parte del controlador.

---
+++ Barberia/src/main/java/poo/barberia/AppointScheduler.java
@@ -0,0 +1,75 @@
+/*
+ * Autora: Tamara Nicole Rodríguez Luna - 2021077818.
+ */
+package poo.barberia;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AppointScheduler {
+    private Map<String,Cliente> clientes;
+    private ArrayList<Cliente> colaEspera;
+    
+    AppointScheduler(){
+        this.clientes = new HashMap<String,Cliente>();
+        this.colaEspera = new ArrayList<Cliente>();
+    }
+    
+    //Métodos de Clientes
+    public void crearCliente(String email, String nombre, String apellido, String telefono) throws Exception{
+        Cliente c = new Cliente(email, nombre, apellido, telefono);
+        clientes.put(email, c);
+    }
+    
+    public void modificarCliente(String email, String nombre, String apellido, String telefono) throws Exception{
+        Cliente c = consultarCliente(email);
+        c.modificarCliente(email, nombre, apellido, telefono);
+    }
+    
+    public void eliminarCliente(String email) throws Exception{
+        if(existeCliente(email) && existeClienteColaEspera(email) && true){
+            // Falta validar que no tenga citas asignadas en el futuro
+            clientes.remove(email);
+        } else {
+            throw new Exception("Cliente no eliminado.");
+        }
+    }
+    
+    public boolean existeCliente(String email){
+        return clientes.containsKey(email);
+    }
+    
+    public Cliente consultarCliente(String email) throws Exception{
+        Cliente c = clientes.get(email);
+        if(c!=null){
+            return c;
+        } else {
+            throw new Exception("Cliente no encontrado.");
+        }
+    }
+    
+    //Métodos de Cola de Espera
+    public ArrayList<Cliente> mostrarColaEspera(){
+        return colaEspera;
+    }
+    
+    public void encolarCliente(Cliente cliente) throws Exception{
+        colaEspera.add(cliente);
+    }
+    
+    public Cliente desencolarCliente(String email) throws Exception{
+        Cliente c = consultarCliente(email);
+        colaEspera.remove(c);
+        return c;
+    }
+    
+    public boolean existeClienteColaEspera(String email){
+        for(int i=0; i < colaEspera.size(); i++){
+            if (colaEspera.get(i).getEmail().equals(email)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Barberia/src/main/java/poo/barberia/Cliente.java
@@ -0,0 +1,85 @@
+/*
+ * Autora: Tamara Nicole Rodríguez Luna - 2021077818.
+ */
+package poo.barberia;
+
+import java.util.regex.Pattern;
+
+public class Cliente {
+    private String email;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    
+    Cliente(String email, String nombre, String apellido, String telefono) throws Exception{
+        boolean emailValido = validEmail(email);
+        boolean telefonoValido = validPhone(telefono);
+        if(emailValido && telefonoValido){
+            this.email = email;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+        } else if (!emailValido) {
+            throw new Exception("Email inválido.");
+        } else {
+            throw new Exception("Número de teléfono inválido.");
+        }
+    }
+    
+    public void modificarCliente(String email, String nombre, String apellido, String telefono) throws Exception{
+        boolean emailValido = validEmail(email);
+        boolean telefonoValido = validPhone(telefono);
+        if(emailValido && telefonoValido){
+            this.email = email;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.telefono = telefono;
+        } else if (!emailValido) {
+            throw new Exception("Email inválido.");
+        } else {
+            throw new Exception("Número de teléfono inválido.");
+        }
+    }
+    
+    public boolean validEmail(String email){
+        String regexEmail = "[^@]+@+[^@]+\\.[a-z]{2,}";
+        return Pattern.matches(regexEmail, email);
+    }
+    
+    public boolean validPhone(String telefono){
+        String regexPhone = "[0-9()+\\-]+";
+        return Pattern.matches(regexPhone, telefono);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
